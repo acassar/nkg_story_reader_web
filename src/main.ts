20bc244a77c769ccaa8067a6d2dc621cf8c54600
Adrien Cassar <adricass@live.fr>
2025-06-22 18:18:16 +0200

    add primeVue

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
+import 'primeflex/primeflex.css'
 import { createPinia } from 'pinia'
-
 import { OhVueIcon, addIcons } from 'oh-vue-icons'
 import { BiSendFill, BiGearFill, LaUndoSolid } from 'oh-vue-icons/icons'
+import Tooltip from 'primevue/tooltip'
 
 import App from './App.vue'
 import router from './router'
@@ -13,8 +16,18 @@ addIcons(BiSendFill, BiGearFill, LaUndoSolid)
 
 const app = createApp(App)
 
+app.directive('tooltip', Tooltip)
+
 app.component('v-icon', OhVueIcon)
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+    options: {
+      darModeSelector: true,
+    },
+  },
+})
 
 app.mount('#app')
