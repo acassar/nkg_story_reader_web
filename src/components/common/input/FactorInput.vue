f8241a8668c36a2ba59986cde0e1e6a824da0720
Adrien Cassar <adricass@live.fr>
2025-06-22 18:18:38 +0200

    add answering delay and speed to settings

---
+++ src/components/common/input/FactorInput.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import { InputText, Slider } from 'primevue'
+import { computed } from 'vue'
+
+const props = withDefaults(
+  defineProps<{
+    name: string
+    label: string
+    min?: number
+    max?: number
+    tooltip?: string
+  }>(),
+  { min: 1, max: 10 },
+)
+
+const model = defineModel<number>()
+
+const modelLabel = computed(() => {
+  if (model.value === 1) return 'Temps réel'
+  if (model.value === props.max) return 'Instantané'
+  return model.value?.toString()
+})
+
+const updateAnweringSpeedFactor = (event: FocusEvent) => {
+  const parsedValue = parseInt((event.target as HTMLInputElement).value)
+  if (!isNaN(parsedValue)) model.value = parsedValue
+}
+</script>
+
+<template>
+  <div class="input">
+    <label :for="name">{{ label }}</label>
+    <InputText
+      v-tooltip="tooltip"
+      @blur="updateAnweringSpeedFactor"
+      :model-value="modelLabel"
+    />
+    <Slider v-tooltip="tooltip" :name :min="min" :max="max" v-model="model" />
+  </div>
+</template>
+
+<style scoped>
+.input {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  align-items: center;
+}
+
+.input label {
+  text-align: center;
+}
+
+.input > * {
+  width: 15%;
+}
+</style>
--- src/components/settings/SettingsComponent.vue
+++ src/components/settings/SettingsComponent.vue
@@ -2,12 +2,22 @@
 import { removeSave } from '@/services/saveService'
 import ButtonComponent from '../common/button/ButtonComponent.vue'
 import type { Story } from '@/class/StoryClass'
+import { ref } from 'vue'
+import FactorInput from '../common/input/FactorInput.vue'
 
 defineProps<{
   story: Story
 }>()
 
 const isOpen = defineModel<boolean>({ required: true })
+
+const answeringSpeedTooltip =
+  'Contrôle la vitesse à laquelle le personnage écrit ses réponses. Une valeur plus élevée accélère l’affichage du texte.'
+const answeringDelayTooltip =
+  'Définit combien de temps le personnage sera absent lors de ses tâches. Plus la valeur est élevée, plus il reviendra rapidement.'
+
+const answeringSpeedFactor = ref(1)
+const answeringDelayFactor = ref(1)
 
 defineEmits<{
   (e: 'fullyClosed'): void
@@ -16,11 +26,26 @@ defineEmits<{
 
 <template>
   <transition @after-leave="() => $emit('fullyClosed')" name="slide">
-    <div v-if="isOpen" class="settings-container">
+    <div v-if="isOpen" class="settings-container gap-3">
       <ButtonComponent @click="isOpen = false"> Retour </ButtonComponent>
       <ButtonComponent @click="() => removeSave(story)">
         Effacer la sauvegarde
       </ButtonComponent>
+
+      <div class="flex flex-column gap-4">
+        <FactorInput
+          name="answering-speed"
+          label="Vitesse de réponse"
+          v-model="answeringSpeedFactor"
+          :tooltip="answeringSpeedTooltip"
+        ></FactorInput>
+        <FactorInput
+          name="answering-delay"
+          label="Temps d’absence"
+          :v-model="answeringDelayFactor"
+          :tooltip="answeringDelayTooltip"
+        ></FactorInput>
+      </div>
     </div>
   </transition>
 </template>
